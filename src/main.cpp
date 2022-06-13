6ffdcb29e6daf3fb8655c6d9b64e8b8315317bc6
ottotak <77091655+ottotak@users.noreply.github.com>
2022-06-13 22:14:54 +0900

    [ADD] Added some files
    Signed-off-by: ottotak <77091655+ottotak@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cstdio>
+#include "battery.cpp"
 using namespace std;
+
+
+int batteryRemain=10;
+void init(){
+  batteryRemain=getBatRemain();
+}
+
 int main(){
-  
+  init();  
+  cout << batteryRemain;
 }
